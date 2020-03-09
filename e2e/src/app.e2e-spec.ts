5337a500ce4a9a2b6e880fc5959bd297667ee6e8
Vinit <vinitkumar7070@gmail.com>
2020-03-09 13:35:39 +0530

    updated

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to blog-app!');
+    expect(page.getTitleText()).toEqual('BLOGS');
   });
 
   afterEach(async () => {
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blog-app'`, () => {
+  it(`should have as title 'Blogs'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('blog-app');
+    expect(app.title).toEqual('Blogs');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to blog-app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Blogs');
   });
 });
